6a695e2c02433029062ce52b42ec4d5d01bd13dc
ameliendrez <ameliendrez@gmail.com>
2018-11-28 13:46:16 -0300

    Nuevas animaciones

--- Trabajos-Practicos/TP5/js/gamePage.js
+++ Trabajos-Practicos/TP5/js/gamePage.js
@@ -13,7 +13,7 @@ function mostrarDatos(json) {
   let html = '';
   for (var i = 0; i < 5; i++) {
 
-    html += '<div class="container single-comment">';
+    html += '<div class="container single-comment oculto">';
     html += '<div class="row"><div class="col-1"><div class="thumbnail">';
     html += '<img class="img-fluid user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"></div></div>';
     html += '<div class="col-11"><div class="card"><div class="card-header">';
@@ -24,6 +24,11 @@ function mostrarDatos(json) {
   }
   let jqHTML = $(html);
   $(".comments").html('').append(jqHTML);
+
+  var temporizador = setTimeout(function(){ 
+    $('.single-comment').removeClass('oculto');
+    clearTimeout(temporizador);
+  }, 1500);
 
 }
 
