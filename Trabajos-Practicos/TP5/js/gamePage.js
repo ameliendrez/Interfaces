1448ec6b74a133856de1ca6e5bdef4d5cbf46958
ameliendrez <ameliendrez@gmail.com>
2018-11-28 13:57:09 -0300

    Aparicion de comentarios al mismo tiempo

--- Trabajos-Practicos/TP5/js/gamePage.js
+++ Trabajos-Practicos/TP5/js/gamePage.js
@@ -13,7 +13,7 @@ function mostrarDatos(json) {
   let html = '';
   for (var i = 0; i < 5; i++) {
 
-    html += '<div class="container single-comment oculto">';
+    html += '<div id="c-'+json[i].id+'" class="container single-comment oculto">';
     html += '<div class="row"><div class="col-1"><div class="thumbnail">';
     html += '<img class="img-fluid user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"></div></div>';
     html += '<div class="col-11"><div class="card"><div class="card-header">';
@@ -25,10 +25,22 @@ function mostrarDatos(json) {
   let jqHTML = $(html);
   $(".comments").html('').append(jqHTML);
 
-  var temporizador = setTimeout(function(){ 
-    $('.single-comment').removeClass('oculto');
-    clearTimeout(temporizador);
-  }, 1500);
+  var comentarios = $('.single-comment');
+  var contador = 1000;
+  var suma = 2000;
+
+  $.each(comentarios, function( index, value ) {
+    var temporizador = setTimeout(function(){ 
+      $('.single-comment').removeClass('oculto');
+      $('.single-comment').addClass('aparicion');
+
+      clearTimeout(temporizador);
+    }, contador);
+    contador += suma;
+  });
+
+
+
 
 }
 
